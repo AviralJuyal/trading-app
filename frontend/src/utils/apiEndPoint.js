0a9dbde9875a4b60dc6c03179167f60c6348867f
aviral juyal <aviraljuyal@gmail.com>
2023-10-08 18:23:06 +0530

    apiendpoint

--- frontend/src/utils/apiEndPoint.js
+++ frontend/src/utils/apiEndPoint.js
@@ -1,3 +1,3 @@
-const apiEndPoint = "http://localhost:8080/api/inchApp";
-// const apiEndPoint = "https://trading-app-tyoj.vercel.app/api/inchApp";
+// const apiEndPoint = "http://localhost:8080/api/inchApp";
+const apiEndPoint = "https://trading-app-tyoj.vercel.app/api/inchApp";
 export { apiEndPoint };
